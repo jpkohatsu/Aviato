632b5f66ede9dec118819d49a3cedfd157acf984
Joseph <jafecafe@gmail.com>
2017-07-14 17:38:51 +0000

    indeed api

--- assets/javascript/firebaseAuth.js
+++ assets/javascript/firebaseAuth.js
@@ -15,7 +15,7 @@
   const txtPassword = document.getElementById("txtPassword");
   const btnLogin = document.getElementById("btnLogin");
   const btnSignUp = document.getElementById("btnSignUp");
-  const btnLogout = document.getElementById("btnLogin");
+  const btnLogout = document.getElementById("btnLogout");
   
   //add logn event
   btnLogin.addEventListener("click", e => {
@@ -62,22 +62,22 @@
     });
     
     
-    //Google sign in stuff
-    var provider = new firebase.auth.GoogleAuthProvider();
+//     //Google sign in stuff
+//     var provider = new firebase.auth.GoogleAuthProvider();
   
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+//   firebase.auth().signInWithPopup(provider).then(function(result) {
+//   // This gives you a Google Access Token. You can use it to access the Google API.
+//   var token = result.credential.accessToken;
+//   // The signed-in user info.
+//   var user = result.user;
+//   // ...
+// }).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // The email of the user's account used.
+//   var email = error.email;
+//   // The firebase.auth.AuthCredential type that was used.
+//   var credential = error.credential;
+//   // ...
+// });
