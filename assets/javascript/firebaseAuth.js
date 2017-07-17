84d67d94cf6818e5f8d21cf85775b4f6302c4fef
Joseph <jafecafe@gmail.com>
2017-07-17 14:29:43 +0000

    cleaned a bit in firebaseAuth

--- assets/javascript/firebaseAuth.js
+++ assets/javascript/firebaseAuth.js
@@ -50,9 +50,7 @@
       firebase.auth().signOut(); 
   });
   
-//   btnLogout.addEventListener("click", e => {
-//      firebase.auth().signOut(); 
-//   });
+
   
   //add a realtime listener
     firebase.auth().onAuthStateChanged(firebaseUser => {
@@ -77,29 +75,7 @@
             $("#btnLogout").addClass("hide");
         }
     });
-    
-    
-//     //Google sign in stuff
-//     var provider = new firebase.auth.GoogleAuthProvider();
-  
-//   firebase.auth().signInWithPopup(provider).then(function(result) {
-//   // This gives you a Google Access Token. You can use it to access the Google API.
-//   var token = result.credential.accessToken;
-//   // The signed-in user info.
-//   var user = result.user;
-//   // ...
-  
-//   console.log("signed up with google");
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-// });
+
 
 var provider = new firebase.auth.GoogleAuthProvider();
 
